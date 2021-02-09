e47c4ee9dca932fe654e0094d87167a437054787
shefaligoel136 <goel136shefali@gmail.com>
2021-02-09 13:47:47 +0530

    Added UI, Actions, Reducers

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+// actions are plain JS object where you have to specify the type property or any other data eg movie- arr, user info-obj etc
+
+{
+    type : 'INCREASE_COUNT'
+}
+{
+    type : 'DECREASE_COUNT'
+}
